4cead8c4a27944ca35e4c4a65431d3f1e731a172
jatinkamboj1 <jatinkamboj0010@gmail.com>
2024-10-08 11:54:31 +0530

    Init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Hello World' });    
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
